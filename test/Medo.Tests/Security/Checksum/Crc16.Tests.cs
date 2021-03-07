f5fa5e93fcca3d2da1da2bf16f4108cfed6fe9b8
Josip Medved <jmedved@jmedved.com>
2021-03-06 21:03:53 -0800

    Crc: refactoring test variables

--- test/Medo.Tests/Security/Checksum/Crc16.Tests.cs
+++ test/Medo.Tests/Security/Checksum/Crc16.Tests.cs
@@ -10,64 +10,64 @@ namespace Medo.Tests.Security.Checksum.Crc16 {
         [Fact(DisplayName = "Crc16: Zmodem")]
         public void Zmodem() {
             string expected = "5E1B";
-            Crc16 actualCrc = Crc16.GetZmodem();
-            actualCrc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-            Assert.Equal(expected, actualCrc.HashAsInt16.ToString("X4"));
-            Assert.Equal(expected, BitConverter.ToString(actualCrc.Hash).Replace("-", ""));
+            var crc = Crc16.GetZmodem();
+            crc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+            Assert.Equal(expected, crc.HashAsInt16.ToString("X4"));
+            Assert.Equal(expected, BitConverter.ToString(crc.Hash).Replace("-", ""));
         }
 
         [Fact(DisplayName = "Crc16: Xmodem")]
         public void Xmodem() {
             string expected = "16A3";
-            Crc16 actualCrc = Crc16.GetXmodem();
-            actualCrc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-            Assert.Equal(expected, actualCrc.HashAsInt16.ToString("X4"));
-            Assert.Equal(expected, BitConverter.ToString(actualCrc.Hash).Replace("-", ""));
+            var crc = Crc16.GetXmodem();
+            crc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+            Assert.Equal(expected, crc.HashAsInt16.ToString("X4"));
+            Assert.Equal(expected, BitConverter.ToString(crc.Hash).Replace("-", ""));
         }
 
         [Fact(DisplayName = "Crc16: X25")]
         public void X25() {
             string expected = "CB47";
-            Crc16 actualCrc = Crc16.GetX25();
-            actualCrc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-            Assert.Equal(expected, actualCrc.HashAsInt16.ToString("X4"));
-            Assert.Equal(expected, BitConverter.ToString(actualCrc.Hash).Replace("-", ""));
+            var crc = Crc16.GetX25();
+            crc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+            Assert.Equal(expected, crc.HashAsInt16.ToString("X4"));
+            Assert.Equal(expected, BitConverter.ToString(crc.Hash).Replace("-", ""));
         }
 
         [Fact(DisplayName = "Crc16: Kermit")]
         public void Kermit() {
             string expected = "9839";
-            Crc16 actualCrc = Crc16.GetKermit();
-            actualCrc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-            Assert.Equal(expected, actualCrc.HashAsInt16.ToString("X4"));
-            Assert.Equal(expected, BitConverter.ToString(actualCrc.Hash).Replace("-", ""));
+            var crc = Crc16.GetKermit();
+            crc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+            Assert.Equal(expected, crc.HashAsInt16.ToString("X4"));
+            Assert.Equal(expected, BitConverter.ToString(crc.Hash).Replace("-", ""));
         }
 
         [Fact(DisplayName = "Crc16: Ieee")]
         public void Ieee() {
             string expected = "178C";
-            Crc16 actualCrc = Crc16.GetIeee();
-            actualCrc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-            Assert.Equal(expected, actualCrc.HashAsInt16.ToString("X4"));
-            Assert.Equal(expected, BitConverter.ToString(actualCrc.Hash).Replace("-", ""));
+            var crc = Crc16.GetIeee();
+            crc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+            Assert.Equal(expected, crc.HashAsInt16.ToString("X4"));
+            Assert.Equal(expected, BitConverter.ToString(crc.Hash).Replace("-", ""));
         }
 
         [Fact(DisplayName = "Crc16: Ccitt")]
         public void Ccitt() {
             string expected = "DF2E";
-            Crc16 actualCrc = Crc16.GetCcitt();
-            actualCrc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-            Assert.Equal(expected, actualCrc.HashAsInt16.ToString("X4"));
-            Assert.Equal(expected, BitConverter.ToString(actualCrc.Hash).Replace("-", ""));
+            var crc = Crc16.GetCcitt();
+            crc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+            Assert.Equal(expected, crc.HashAsInt16.ToString("X4"));
+            Assert.Equal(expected, BitConverter.ToString(crc.Hash).Replace("-", ""));
         }
 
         [Fact(DisplayName = "Crc16: Arc")]
         public void Arc() {
             string expected = "178C";
-            Crc16 actualCrc = Crc16.GetArc();
-            actualCrc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-            Assert.Equal(expected, actualCrc.HashAsInt16.ToString("X4"));
-            Assert.Equal(expected, BitConverter.ToString(actualCrc.Hash).Replace("-", ""));
+            var crc = Crc16.GetArc();
+            crc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+            Assert.Equal(expected, crc.HashAsInt16.ToString("X4"));
+            Assert.Equal(expected, BitConverter.ToString(crc.Hash).Replace("-", ""));
         }
 
     }
--- test/Medo.Tests/Security/Checksum/Crc8.Tests.cs
+++ test/Medo.Tests/Security/Checksum/Crc8.Tests.cs
@@ -10,19 +10,19 @@ namespace Medo.Tests.Security.Checksum.Crc8 {
         [Fact(DisplayName = "Crc8: Dallas")]
         public void Dallas() {
             string expected = "80";
-            Crc8 actualCrc = Crc8.GetDallas();
-            actualCrc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-            Assert.Equal(expected, actualCrc.HashAsByte.ToString("X2"));
-            Assert.Equal(expected, BitConverter.ToString(actualCrc.Hash).Replace("-", ""));
+            var crc = Crc8.GetDallas();
+            crc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+            Assert.Equal(expected, crc.HashAsByte.ToString("X2"));
+            Assert.Equal(expected, BitConverter.ToString(crc.Hash).Replace("-", ""));
         }
 
         [Fact(DisplayName = "Crc8: Maxim")]
         public void Maxim() {
             string expected = "80";
-            Crc8 actualCrc = Crc8.GetDallas();
-            actualCrc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
-            Assert.Equal(expected, actualCrc.HashAsByte.ToString("X2"));
-            Assert.Equal(expected, BitConverter.ToString(actualCrc.Hash).Replace("-", ""));
+            var crc = Crc8.GetDallas();
+            crc.ComputeHash(Encoding.ASCII.GetBytes("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
+            Assert.Equal(expected, crc.HashAsByte.ToString("X2"));
+            Assert.Equal(expected, BitConverter.ToString(crc.Hash).Replace("-", ""));
         }
 
     }
