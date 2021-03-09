222a33819d97621a14ea936c6be04e3ce6dd165a
Josip Medved <jmedved@jmedved.com>
2021-03-08 20:46:59 -0800

    ParameterExpansion: initial

---
+++ src/Medo/Text/ParameterExpansion.cs
@@ -0,0 +1,148 @@
+/* Josip Medved <jmedved@jmedved.com> * www.medo64.com * MIT License */
+
+namespace Medo.Text {
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// Performs basic shell parameter expansion.
+    /// </summary>
+    /// <remarks>https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html</remarks>
+    public sealed class ParameterExpansion {
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        public ParameterExpansion() {
+        }
+
+
+        /// <summary>
+        /// Returns expanded text.
+        /// </summary>
+        /// <param name="text">Text to expand.</param>
+        /// <returns></returns>
+        public string Expand(string text) {
+            if (text == null) { throw new ArgumentNullException(nameof(text), "Text cannot be null."); }
+
+            var state = State.Text;
+            var sbOutput = new StringBuilder();
+            var sbParameterName = new StringBuilder();
+
+            var queue = new Queue<char?>();
+            foreach (var ch in text) {
+                queue.Enqueue(ch);
+            }
+            queue.Enqueue(null);  // marker for end of parsing
+
+            while (queue.Count > 0) {
+                var ch = queue.Dequeue();
+
+                switch (state) {
+                    case State.Text: {
+                            if (ch == '$') {
+                                sbParameterName.Clear();
+                                state = State.ParameterStart;
+                            } else {
+                                sbOutput.Append(ch);
+                            }
+                        }
+                        break;
+
+                    case State.ParameterStart: {
+                            if (ch == '{') {  // start complex parameter
+                                state = State.ComplexParameter;
+                            } else if (ch.HasValue && (char.IsLetterOrDigit(ch.Value) || (ch == '_'))) {  // normal variable
+                                sbParameterName.Append(ch);
+                                state = State.SimpleParameter;
+                            } else {  // just assume it's normal text
+                                sbOutput.Append('$');
+                                sbOutput.Append(ch);
+                                state = State.Text;
+                            }
+                        }
+                        break;
+
+                    case State.SimpleParameter: {
+                            if (ch.HasValue && (char.IsLetterOrDigit(ch.Value) || (ch == '_'))) {  // continue as variable
+                                sbParameterName.Append(ch);
+                            } else {  // parameter done
+                                OnRetrieveParameter(sbParameterName.ToString(), out var value);
+                                sbOutput.Append(value);
+                                sbOutput.Append(ch);
+                                state = State.Text;
+                            }
+                        }
+                        break;
+
+                    case State.ComplexParameter: {
+                            if (ch == '}') {  // parameter done
+                                OnRetrieveParameter(sbParameterName.ToString(), out var value);
+                                sbOutput.Append(value);
+                                state = State.Text;
+                            } else {
+                                sbParameterName.Append(ch);
+                            }
+                        }
+                        break;
+
+                }
+            }
+
+            return sbOutput.ToString();
+        }
+
+
+        private void OnRetrieveParameter(string name, out string? value) {
+            var e = new ParameterExpansionEventArgs(name);
+            RetrieveParameter?.Invoke(this, e);
+            value = e.Value;
+        }
+
+        /// <summary>
+        /// Evenr raised to gather parameter values.
+        /// </summary>
+        public event EventHandler<ParameterExpansionEventArgs>? RetrieveParameter;
+
+
+        #region State
+
+        private enum State {
+            Text, ParameterStart, SimpleParameter, ComplexParameter,
+        }
+
+        #endregion State
+
+    }
+
+
+
+    /// <summary>
+    /// Event arguments for parameter expansion.
+    /// </summary>
+    public class ParameterExpansionEventArgs : EventArgs {
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <param name="parameterName">Parameter name.</param>
+        /// <exception cref="ArgumentNullException">Parameter name cannot be null.</exception>
+        public ParameterExpansionEventArgs(string parameterName) {
+            if (parameterName == null) { throw new ArgumentNullException(nameof(parameterName), "Parameter name cannot be null."); }
+            Name = parameterName;
+        }
+
+        /// <summary>
+        /// Gets parameter name.
+        /// </summary>
+        public string Name { get; init; }
+
+        /// <summary>
+        /// Gets/sets parameter value.
+        /// If null, parameter will be assumed missing.
+        /// </summary>
+        public string? Value { get; set; }
+
+    }
+}
---
+++ test/Medo.Tests/Text/ParameterExpansion.Tests.cs
@@ -0,0 +1,112 @@
+using System;
+using Xunit;
+
+namespace Medo.Tests.Text.ParameterExpansion {
+    using Medo.Text;
+
+    public class Tests {
+
+        [Fact(DisplayName = "ParameterExpansion: Empty")]
+        public void Empty() {
+            var shell = new ParameterExpansion();
+            var output = shell.Expand("");
+            Assert.Equal("", output);
+        }
+
+        [Fact(DisplayName = "ParameterExpansion: Only text")]
+        public void OnlyText() {
+            var shell = new ParameterExpansion();
+            var output = shell.Expand("Test");
+            Assert.Equal("Test", output);
+        }
+
+        [Fact(DisplayName = "ParameterExpansion: Lone dollar (1)")]
+        public void LoneDollar1() {
+            var shell = new ParameterExpansion();
+            var output = shell.Expand("$");
+            Assert.Equal("$", output);
+        }
+
+        [Fact(DisplayName = "ParameterExpansion: Lone dollar (2)")]
+        public void LoneDollar2() {
+            var shell = new ParameterExpansion();
+            var output = shell.Expand("#$-");
+            Assert.Equal("#$-", output);
+        }
+
+        [Fact(DisplayName = "ParameterExpansion: Cannot expand (1)")]
+        public void CannotExpand1() {
+            var shell = new ParameterExpansion();
+            var output = shell.Expand("$X");
+            Assert.Equal("", output);
+        }
+
+        [Fact(DisplayName = "ParameterExpansion: Cannot expand (2)")]
+        public void CannotExpand() {
+            var shell = new ParameterExpansion();
+            var output = shell.Expand("-$X-");
+            Assert.Equal("--", output);
+        }
+
+        [Fact(DisplayName = "ParameterExpansion: Simple expand (1)")]
+        public void SimpleExpand() {
+            var shell = new ParameterExpansion();
+            shell.RetrieveParameter += delegate (object sender, ParameterExpansionEventArgs e) {
+                e.Value = (e.Name == "X") ? "Y" : null;
+            };
+            var output = shell.Expand("$X");
+            Assert.Equal("Y", output);
+        }
+
+        [Fact(DisplayName = "ParameterExpansion: Simple expand (2)")]
+        public void SimpleExpand2() {
+            var shell = new ParameterExpansion();
+            shell.RetrieveParameter += delegate (object sender, ParameterExpansionEventArgs e) {
+                e.Value = (e.Name == "X") ? "Y" : null;
+            };
+            var output = shell.Expand("-$X-");
+            Assert.Equal("-Y-", output);
+        }
+
+        [Fact(DisplayName = "ParameterExpansion: Special chars")]
+        public void SpecialChars() {
+            var shell = new ParameterExpansion();
+            shell.RetrieveParameter += delegate (object sender, ParameterExpansionEventArgs e) {
+                e.Value = (e.Name == "X") ? "\n" : null;
+            };
+            var output = shell.Expand("\t$X\r\0");
+            Assert.Equal("\t\n\r\0", output);
+        }
+
+        [Fact(DisplayName = "ParameterExpansion: Complex expand (1)")]
+        public void ComplexExpand1() {
+            var shell = new ParameterExpansion();
+            shell.RetrieveParameter += delegate (object sender, ParameterExpansionEventArgs e) {
+                e.Value = (e.Name == "X") ? "Y" : null;
+            };
+            var output = shell.Expand("${X}");
+            Assert.Equal("Y", output);
+        }
+
+        [Fact(DisplayName = "ParameterExpansion: Complex expand (2)")]
+        public void ComplexExpand2() {
+            var shell = new ParameterExpansion();
+            shell.RetrieveParameter += delegate (object sender, ParameterExpansionEventArgs e) {
+                e.Value = (e.Name == "X") ? "Y" : null;
+            };
+            var output = shell.Expand("A${X}B");
+            Assert.Equal("AYB", output);
+        }
+
+        [Fact(DisplayName = "ParameterExpansion: Incomplete complex expand")]
+        public void IncompleteComplexExpand() {
+            var shell = new ParameterExpansion();
+            shell.RetrieveParameter += delegate (object sender, ParameterExpansionEventArgs e) {
+                e.Value = (e.Name == "X") ? "Y" : null;
+            };
+            var output = shell.Expand("A${X");
+            Assert.Equal("A", output);
+        }
+
+    }
+}
