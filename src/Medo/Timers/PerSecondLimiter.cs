5d7c2c6091b7583a84f6e6d1fd720b8e2146bad6
Josip Medved <jmedved@jmedved.com>
2021-03-10 22:40:13 -0800

    PerSecondLimiter: refactored to use SemaphoreSlim

--- src/Medo/Timers/PerSecondLimiter.cs
+++ src/Medo/Timers/PerSecondLimiter.cs
@@ -2,8 +2,8 @@
 
 namespace Medo.Timers {
     using System;
-    using System.Diagnostics;
     using System.Threading;
+    using System.Threading.Tasks;
 
     /// <summary>
     /// Limits per-second throughput.
@@ -15,26 +15,26 @@ namespace Medo.Timers {
         /// </summary>
         /// <param name="perSecondrate">Number of requests targeted per-second.</param>
         /// <exception cref="ArgumentOutOfRangeException">Per-second rate cannot be lower than 0.</exception>
-        public PerSecondLimiter(long perSecondRate) {
+        public PerSecondLimiter(int perSecondRate) {
             if (perSecondRate < 0) { throw new ArgumentOutOfRangeException(nameof(perSecondRate), "Per-second rate cannot be lower than 0."); }
             PerSecondRate = perSecondRate;
+            HeartbeatTimer = new Timer(Heartbeat, null, 0, 1);
         }
 
 
-        private long _perSecondRate;
+        private int _perSecondRate;
         /// <summary>
         /// Gets/sets per-second rate.
         /// </summary>
-        public long PerSecondRate {
+        public int PerSecondRate {
             get { return _perSecondRate; }
             set {
                 if (value < 0) { throw new ArgumentOutOfRangeException(nameof(value), "Per-second rate cannot be lower than 0."); }
-                lock (SyncRoot) {
-                    _perSecondRate = value;
-
-                    var maxTimeout = (int)((long)1000 / (value + 1));
-                    WaitMillisecondsTimeout = maxTimeout > 0 ? maxTimeout : 1;
-                    PerSecondRateCarryOver = value / 20;  // 5%
+                lock (SyncTimer) {
+                    lock (SyncProperties) {
+                        _perSecondRate = value;
+                    }
+                    while (Tickets.CurrentCount > 0) { Tickets.Wait(0); }  // use up remaining allowance
                 }
             }
         }
@@ -60,82 +60,102 @@ namespace Medo.Timers {
         /// <summary>
         /// Returns true once the next action can be executed.
         /// Once true is returned, it will be assumed action is taken.
-        /// Returns false once timeout has elapsed.
+        /// Timeout of 0 will exit immediatelly if the next action cannot be executed.
         /// </summary>
-        /// <param name="maximumWait">How many milliseconds to wait or Timeout.Infinite to wait forever.</param>
-        public bool WaitForNext(int maximumWait) {
-            if (maximumWait < -1) { throw new ArgumentOutOfRangeException(nameof(maximumWait), "Invalid timeout."); }
+        /// <param name="millisecondTimeout">How many milliseconds to wait or Timeout.Infinite to wait forever.</param>
+        public bool WaitForNext(int millisecondTimeout) {
+            if (millisecondTimeout < -1) { throw new ArgumentOutOfRangeException(nameof(millisecondTimeout), "Invalid timeout."); }
             if (PerSecondRate == 0) { return true; }  // skip calculations if unlimited
-
-            if (IsNextAvailable()) {  // check if we could return immediatelly
-                return true;
-            } else if (maximumWait == Timeout.Infinite) {  // loop until there's a slot
-                while (!IsNextAvailable()) {
-                    SignalSleep.WaitOne(WaitMillisecondsTimeout);  // wait a bit before checking it again (one to six 15ms intervals)
-                }
-                return true;
-            } else if (maximumWait > 0) {  // check occasionally until timeout is reached
-                var sw = new Stopwatch();
-                sw.Start();
-                while (sw.ElapsedMilliseconds < maximumWait) {
-                    if (IsNextAvailable()) { return true; }
-                    SignalSleep.WaitOne(WaitMillisecondsTimeout);  // wait a bit before checking it again (one to six 15ms intervals)
-                }
-            }
-
-            return false;  // give up
+            return Tickets.Wait(millisecondTimeout);
         }
 
 
-        #region Internal
-
-        private bool IsNextAvailable() {
-            lock (SyncRoot) {
-                var ticks = DateTime.UtcNow.Ticks;
-                var tickSeconds = ticks / 10000000;
-                var fraction = (ticks / 10000) % 1000;
-
-                if (tickSeconds == CurrTickSeconds + 1) {  // this is preceeding second
-                    CurrTickSeconds = tickSeconds;
-                    var leftover = (PerSecondRate - CurrAccumulator);
-                    if (leftover > PerSecondRateCarryOver) {
-                        leftover = PerSecondRateCarryOver;  // limit how much you can carry over
-                    } else if (leftover < 0) {
-                        leftover = 0;  // ignore leftover
-                    }
-                    CurrAccumulator = 1 - leftover;  // if any leftover, adjust accumulator
-                    return true;
-                } else if (CurrTickSeconds != tickSeconds) {  // previous action was more than 1 second ago
-                    CurrTickSeconds = tickSeconds;
-                    CurrAccumulator = 1;
-                    return true;  // always allow when switching time intervals
-                }
-
-                var fractionRate = CurrAccumulator * 1000 / PerSecondRate;
-                if (fractionRate <= fraction) {
-                    CurrAccumulator += 1;
-                    if (PerSecondRate > 50) {  // don't bother with signalling if rate is low
-                        var newFractionRate = CurrAccumulator * 1000 / PerSecondRate;  // check if rate allows for more TPS
-                        if (newFractionRate <= fraction) { SignalSleep.Set(); }  // wake someone early
-                    }
-                    return true;
-                }
-            }
-
-
-            return false;
+        /// <summary>
+        /// Returns true once the next action can be executed.
+        /// Once true is returned, it will be assumed action is taken.
+        /// </summary>
+        public Task<bool> WaitForNextAsync() {
+            return WaitForNextAsync(Timeout.Infinite);
         }
 
-        private readonly object SyncRoot = new();
-        private readonly AutoResetEvent SignalSleep = new(false);
+        /// <summary>
+        /// Returns true once the next action can be executed.
+        /// Once true is returned, it will be assumed action is taken.
+        /// Timeout of 0 will exit immediatelly if the next action cannot be executed.
+        /// </summary>
+        /// <param name="millisecondTimeout">How many milliseconds to wait or Timeout.Infinite to wait forever.</param>
+        public async Task<bool> WaitForNextAsync(int millisecondTimeout) {
+            if (millisecondTimeout < -1) { throw new ArgumentOutOfRangeException(nameof(millisecondTimeout), "Invalid timeout."); }
+            if (PerSecondRate == 0) { return true; }  // skip calculations if unlimited
+            return await Tickets.WaitAsync(Timeout.Infinite);
+        }
 
-        private int WaitMillisecondsTimeout;
-        private long PerSecondRateCarryOver;
 
-        private long CurrTickSeconds = 0;
-        private long CurrAccumulator = 0;
+        #region Timer
 
-        #endregion Internal
+#pragma warning disable IDE0052 // Remove unread private members
+        private readonly Timer HeartbeatTimer;
+#pragma warning restore IDE0052 // Remove unread private members
+
+        private readonly object SyncProperties = new();
+        private readonly object SyncTimer = new();
+        private readonly SemaphoreSlim Tickets = new(0);
+
+        private void Heartbeat(object? state) {
+            lock (SyncTimer) {
+                var now = Environment.TickCount;
+                var msLastElapsed = unchecked(now - PeriodLastStart);
+                if (msLastElapsed == 0) { return; }  // less than 1ms has elapsed - can happen sometime
+
+                try {
+                    long rate;
+                    lock (SyncProperties) {
+                        rate = PerSecondRate;
+                    }
+
+                    if (rate == 0) { return; }  // special case when semaphore is not used
+
+                    var allowanceRemaining = Tickets.CurrentCount;
+                    var msTotalElapsed = unchecked(now - PeriodTotalStart);
+
+                    if (msTotalElapsed >= 2000) {  // it's been long enough to forget all about counting
+                        //System.Diagnostics.Debug.WriteLine($"[Medo PerSecondLimiter] Init");
+                        PeriodTotalStart = now;
+                        PeriodTotalAllowanceUsed = 0;
+                    } else {  // try to distribute quota bit by bit
+                        //System.Diagnostics.Debug.WriteLine($"[Medo PerSecondLimiter] Tick");
+                        if (allowanceRemaining > 0) { return; }  // if there is unused allowance, don't give it more
+
+                        var expectedLastUsage = (rate + 1) * msLastElapsed / 1000 + 1;
+                        var expectedTotalUsage = rate * msTotalElapsed / 1000 + 1;
+
+                        var rateMaxUnused = rate / 16;  // limit unused counter to about 6.25% at a time
+                        if (rateMaxUnused == 0) { rateMaxUnused = 1; }  // always allow at least one
+                        var unused = expectedTotalUsage - PeriodTotalAllowanceUsed;
+                        if (unused > rateMaxUnused) { unused = rateMaxUnused; }
+                        if (unused > 0) {
+                            PeriodTotalAllowanceUsed += unused;
+                            Tickets.Release((int)unused);
+                            //System.Diagnostics.Debug.WriteLine($"[Medo PerSecondLimiter] Tick: Allowed {unused} for {PeriodTotalAllowanceUsed}/{rate} at {msTotalElapsed}ms ({msLastElapsed}ms) {{{now}}})");
+                        }
+
+                        if (msTotalElapsed >= 1000) {  // move sliding window to the current second
+                            PeriodTotalStart += 1000;
+                            PeriodTotalAllowanceUsed -= rate;
+                            if (PeriodTotalAllowanceUsed < 0) { PeriodTotalAllowanceUsed = 0; }  // forget about missed
+                        }
+                    }
+                } finally {
+                    PeriodLastStart = now;
+                }
+            }
+        }
+
+        private long PeriodLastStart;
+        private long PeriodTotalStart;
+        private long PeriodTotalAllowanceUsed;
+
+        #endregion Timer
 
     }
 }
