baa7d32cd8ed7aa01a9ad324cd650363c378323a
Josip Medved <jmedved@jmedved.com>
2021-10-31 21:13:30 -0700

    LifetimeWatch: Updated tests

--- tests/Tests.Medo/Diagnostics/LifetimeWatch.Tests.cs
+++ tests/Tests.Medo/Diagnostics/LifetimeWatch.Tests.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using Medo.Diagnostics;
+using System.Threading;
 
 namespace Tests.Medo.Diagnostics {
     public class LifetimeWatchTests {
@@ -7,10 +8,10 @@ namespace Tests.Medo.Diagnostics {
         [Fact(DisplayName = "LifetimeWatch: Basic")]
         public void Basic() {
             var lifetime = new LifetimeWatch();
-            var t1 = lifetime.ElapsedTicks;
-            var t2 = lifetime.ElapsedTicks;
+            var t1 = lifetime.ElapsedTicks; Thread.Sleep(1);
+            var t2 = lifetime.ElapsedTicks; Thread.Sleep(1);
             lifetime.Dispose();
-            var t3 = lifetime.ElapsedTicks;
+            var t3 = lifetime.ElapsedTicks; Thread.Sleep(1);
             var t4 = lifetime.ElapsedTicks;
             Assert.True(t2 > t1);
             Assert.True(t3 > t2);
@@ -20,11 +21,11 @@ namespace Tests.Medo.Diagnostics {
         [Fact(DisplayName = "LifetimeWatch: Double dispose")]
         public void DoubleDisposeOk() {
             var lifetime = new LifetimeWatch();
-            var t1 = lifetime.ElapsedTicks;
-            var t2 = lifetime.ElapsedTicks;
+            var t1 = lifetime.ElapsedTicks; Thread.Sleep(1);
+            var t2 = lifetime.ElapsedTicks; Thread.Sleep(1);
             lifetime.Dispose();
             lifetime.Dispose();
-            var t3 = lifetime.ElapsedTicks;
+            var t3 = lifetime.ElapsedTicks; Thread.Sleep(1);
             lifetime.Dispose();
             var t4 = lifetime.ElapsedTicks;
             Assert.True(t2 > t1);
