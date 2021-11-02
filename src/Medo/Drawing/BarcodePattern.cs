9290d9f0d5bbc1aac618a48ab3c563ad3a1a315f
Josip Medved <jmedved@jmedved.com>
2021-11-01 22:45:49 -0700

    BarcodePattern: Added PNG output (WritePngImageToStream method)

--- src/Medo/Drawing/BarcodePattern.cs
+++ src/Medo/Drawing/BarcodePattern.cs
@@ -1,5 +1,6 @@
 /* Josip Medved <jmedved@jmedved.com> * www.medo64.com * MIT License */
 
+//2021-11-01: Added PNG output (WritePngImageToStream method)
 //2021-10-31: Refactored for .NET 5
 //            Removed System.Drawing dependency
 //2008-12-09: Changed Code 128 pattern 27 from 311212 to 312212
@@ -10,7 +11,11 @@
 namespace Medo.Drawing {
     using System;
     using System.Collections.Generic;
+    using System.Drawing;
     using System.Globalization;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Text;
 
     /// <summary>
     /// Barcode drawing.
@@ -101,6 +106,159 @@ namespace Medo.Drawing {
             }
         }
 
+
+        /// <summary>
+        /// Writes PNG image to a stream.
+        /// </summary>
+        /// <param name="stream">Stream that will be written to.</param>
+        /// <exception cref="ArgumentNullException">Stream cannot be null.</exception>
+        public void WritePngImageToStream(Stream stream) {
+            WritePngImageToStream(stream, Color.Black, Color.White);
+        }
+
+        /// <summary>
+        /// Writes PNG image to a stream.
+        /// </summary>
+        /// <param name="stream">Stream that will be written to.</param>
+        /// <param name="barColor">Bar color.</param>
+        /// <param name="gapColor">Color of gaps and spaces.</param>
+        /// <exception cref="ArgumentNullException">Stream cannot be null.</exception>
+        public void WritePngImageToStream(Stream stream, Color barColor, Color gapColor) {
+            WritePngImageToStream(stream, barColor, gapColor, 1, 9, 3);
+        }
+
+        /// <summary>
+        /// Writes PNG image to a stream.
+        /// </summary>
+        /// <param name="stream">Stream that will be written to.</param>
+        /// <param name="barColor">Bar color.</param>
+        /// <param name="gapColor">Color of gaps and spaces.</param>
+        /// <param name="barWidth">Width of a single bar.</param>
+        /// <param name="barHeight">Height of a single bar.</param>
+        /// <param name="margin">Width of margin around barcode.</param>
+        /// <exception cref="ArgumentNullException">Stream cannot be null.</exception>
+        /// <exception cref="ArgumentOutOfRangeException">Bar width must be between 1 and 100 pixels. -or- Bar height must be between 1 and 1000 pixels. -or- Margin must be between 1 and 1000 pixels.</exception>
+        public void WritePngImageToStream(Stream stream, Color barColor, Color gapColor, int barWidth, int barHeight, int margin) {
+            if (stream == null) { throw new ArgumentNullException(nameof(stream), "Stream cannot be null."); }
+            if ((barWidth <= 0) || (barWidth > 100)) { throw new ArgumentOutOfRangeException(nameof(barWidth), "Bar width must be between 1 and 100 pixels."); }
+            if ((barHeight <= 0) || (barHeight > 1000)) { throw new ArgumentOutOfRangeException(nameof(barHeight), "Bar height must be between 1 and 1000 pixels."); }
+            if ((margin < 0) || (margin > 1000)) { throw new ArgumentOutOfRangeException(nameof(margin), "Margin must be between 1 and 1000 pixels."); }
+
+            var hasTransparency = (barColor.A < 255) || (gapColor.A < 255);
+            var barBytes = hasTransparency ? new byte[] { barColor.R, barColor.G, barColor.B, barColor.A } : new byte[] { barColor.R, barColor.G, barColor.B };
+            var gapBytes = hasTransparency ? new byte[] { gapColor.R, gapColor.G, gapColor.B, gapColor.A } : new byte[] { gapColor.R, gapColor.G, gapColor.B };
+
+            var memStream = new MemoryStream();  // prepare data
+            memStream.Write(new byte[] { 0x78, 0x9C });  // deflate header
+            var deflateStream = new DeflateStream(memStream, CompressionLevel.Optimal);
+
+            var barcodePattern = GetBinaryPattern();
+            var totalWidth = (uint)(margin + barcodePattern.Length + margin);
+            var totalHeight = (uint)(margin + barHeight + margin);
+
+            for (var y = 0; y < margin; y++) {  // top margin
+                deflateStream.Write(new byte[] { 0x00 }, 0, 1);  // start the line with no filter
+                for (var x = 0; x < totalWidth; x++) { deflateStream.Write(gapBytes); }
+            }
+
+            for (var y = 0; y < barHeight; y++) {  // barcode
+                deflateStream.Write(new byte[] { 0x00 }, 0, 1);  // start the line with no filter
+                for (var x = 0; x < margin; x++) { deflateStream.Write(gapBytes); }  // left margin
+                for (var x = 0; x < barcodePattern.Length; x++) {
+                    deflateStream.Write(barcodePattern[x] ? barBytes : gapBytes);
+                }
+                for (var x = 0; x < margin; x++) { deflateStream.Write(gapBytes); }  // right margin
+            }
+
+            for (var y = 0; y < margin; y++) {  // bottom margin
+                deflateStream.Write(new byte[] { 0x00 }, 0, 1);  // start the line with no filter
+                for (var x = 0; x < totalWidth; x++) { deflateStream.Write(gapBytes); }
+            }
+
+            deflateStream.Close();
+
+            stream.Write(new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A }, 0, 8);  // Header
+            WritePngChunk(stream, Encoding.ASCII.GetBytes("IHDR"), GetBEBytes(totalWidth), GetBEBytes(totalHeight), new byte[] { 0x08, (byte)(hasTransparency ? 0x06 : 0x02), 0x00, 0x00, 0x00 });
+            WritePngChunk(stream, Encoding.ASCII.GetBytes("IDAT"), memStream.ToArray());
+            WritePngChunk(stream, Encoding.ASCII.GetBytes("IEND"));
+        }
+
+        #region Png
+
+        private static void WritePngChunk(Stream stream, byte[] chunkNameBytes, params byte[][] dataBytes) {
+            var totalLength = 0U;
+            foreach (var dataBuffer in dataBytes) {
+                totalLength += (uint)dataBuffer.Length;
+            }
+
+            var chunkLenghtBytes = GetBEBytes(totalLength);
+            stream.Write(chunkLenghtBytes, 0, chunkLenghtBytes.Length);
+
+            stream.Write(chunkNameBytes, 0, chunkNameBytes.Length);
+            var crc = CalculateCrc32(0xFFFFFFFF, chunkNameBytes);  // start CRC calculation with chunk name
+
+            foreach (var dataBuffer in dataBytes) {
+                stream.Write(dataBuffer, 0, dataBuffer.Length);
+                crc = CalculateCrc32(crc, dataBuffer);  // add data bytes to CRC
+            }
+
+            var crcBytes = GetBEBytes(crc ^ 0xFFFFFFFF);  // final CRC xor and convert to bytes
+            stream.Write(crcBytes, 0, crcBytes.Length);
+        }
+
+        private static byte[] GetBEBytes(uint number) {
+            if (BitConverter.IsLittleEndian) {
+                var buffer = BitConverter.GetBytes(number);
+                return new byte[] { buffer[3], buffer[2], buffer[1], buffer[0] };
+            } else {
+                return BitConverter.GetBytes(number);
+            }
+        }
+
+        private static readonly uint[] Crc32LookupTable = {
+            0x00000000, 0x77073096, 0xEE0E612C, 0x990951BA, 0x076DC419, 0x706AF48F, 0xE963A535, 0x9E6495A3,
+            0x0EDB8832, 0x79DCB8A4, 0xE0D5E91E, 0x97D2D988, 0x09B64C2B, 0x7EB17CBD, 0xE7B82D07, 0x90BF1D91,
+            0x1DB71064, 0x6AB020F2, 0xF3B97148, 0x84BE41DE, 0x1ADAD47D, 0x6DDDE4EB, 0xF4D4B551, 0x83D385C7,
+            0x136C9856, 0x646BA8C0, 0xFD62F97A, 0x8A65C9EC, 0x14015C4F, 0x63066CD9, 0xFA0F3D63, 0x8D080DF5,
+            0x3B6E20C8, 0x4C69105E, 0xD56041E4, 0xA2677172, 0x3C03E4D1, 0x4B04D447, 0xD20D85FD, 0xA50AB56B,
+            0x35B5A8FA, 0x42B2986C, 0xDBBBC9D6, 0xACBCF940, 0x32D86CE3, 0x45DF5C75, 0xDCD60DCF, 0xABD13D59,
+            0x26D930AC, 0x51DE003A, 0xC8D75180, 0xBFD06116, 0x21B4F4B5, 0x56B3C423, 0xCFBA9599, 0xB8BDA50F,
+            0x2802B89E, 0x5F058808, 0xC60CD9B2, 0xB10BE924, 0x2F6F7C87, 0x58684C11, 0xC1611DAB, 0xB6662D3D,
+            0x76DC4190, 0x01DB7106, 0x98D220BC, 0xEFD5102A, 0x71B18589, 0x06B6B51F, 0x9FBFE4A5, 0xE8B8D433,
+            0x7807C9A2, 0x0F00F934, 0x9609A88E, 0xE10E9818, 0x7F6A0DBB, 0x086D3D2D, 0x91646C97, 0xE6635C01,
+            0x6B6B51F4, 0x1C6C6162, 0x856530D8, 0xF262004E, 0x6C0695ED, 0x1B01A57B, 0x8208F4C1, 0xF50FC457,
+            0x65B0D9C6, 0x12B7E950, 0x8BBEB8EA, 0xFCB9887C, 0x62DD1DDF, 0x15DA2D49, 0x8CD37CF3, 0xFBD44C65,
+            0x4DB26158, 0x3AB551CE, 0xA3BC0074, 0xD4BB30E2, 0x4ADFA541, 0x3DD895D7, 0xA4D1C46D, 0xD3D6F4FB,
+            0x4369E96A, 0x346ED9FC, 0xAD678846, 0xDA60B8D0, 0x44042D73, 0x33031DE5, 0xAA0A4C5F, 0xDD0D7CC9,
+            0x5005713C, 0x270241AA, 0xBE0B1010, 0xC90C2086, 0x5768B525, 0x206F85B3, 0xB966D409, 0xCE61E49F,
+            0x5EDEF90E, 0x29D9C998, 0xB0D09822, 0xC7D7A8B4, 0x59B33D17, 0x2EB40D81, 0xB7BD5C3B, 0xC0BA6CAD,
+            0xEDB88320, 0x9ABFB3B6, 0x03B6E20C, 0x74B1D29A, 0xEAD54739, 0x9DD277AF, 0x04DB2615, 0x73DC1683,
+            0xE3630B12, 0x94643B84, 0x0D6D6A3E, 0x7A6A5AA8, 0xE40ECF0B, 0x9309FF9D, 0x0A00AE27, 0x7D079EB1,
+            0xF00F9344, 0x8708A3D2, 0x1E01F268, 0x6906C2FE, 0xF762575D, 0x806567CB, 0x196C3671, 0x6E6B06E7,
+            0xFED41B76, 0x89D32BE0, 0x10DA7A5A, 0x67DD4ACC, 0xF9B9DF6F, 0x8EBEEFF9, 0x17B7BE43, 0x60B08ED5,
+            0xD6D6A3E8, 0xA1D1937E, 0x38D8C2C4, 0x4FDFF252, 0xD1BB67F1, 0xA6BC5767, 0x3FB506DD, 0x48B2364B,
+            0xD80D2BDA, 0xAF0A1B4C, 0x36034AF6, 0x41047A60, 0xDF60EFC3, 0xA867DF55, 0x316E8EEF, 0x4669BE79,
+            0xCB61B38C, 0xBC66831A, 0x256FD2A0, 0x5268E236, 0xCC0C7795, 0xBB0B4703, 0x220216B9, 0x5505262F,
+            0xC5BA3BBE, 0xB2BD0B28, 0x2BB45A92, 0x5CB36A04, 0xC2D7FFA7, 0xB5D0CF31, 0x2CD99E8B, 0x5BDEAE1D,
+            0x9B64C2B0, 0xEC63F226, 0x756AA39C, 0x026D930A, 0x9C0906A9, 0xEB0E363F, 0x72076785, 0x05005713,
+            0x95BF4A82, 0xE2B87A14, 0x7BB12BAE, 0x0CB61B38, 0x92D28E9B, 0xE5D5BE0D, 0x7CDCEFB7, 0x0BDBDF21,
+            0x86D3D2D4, 0xF1D4E242, 0x68DDB3F8, 0x1FDA836E, 0x81BE16CD, 0xF6B9265B, 0x6FB077E1, 0x18B74777,
+            0x88085AE6, 0xFF0F6A70, 0x66063BCA, 0x11010B5C, 0x8F659EFF, 0xF862AE69, 0x616BFFD3, 0x166CCF45,
+            0xA00AE278, 0xD70DD2EE, 0x4E048354, 0x3903B3C2, 0xA7672661, 0xD06016F7, 0x4969474D, 0x3E6E77DB,
+            0xAED16A4A, 0xD9D65ADC, 0x40DF0B66, 0x37D83BF0, 0xA9BCAE53, 0xDEBB9EC5, 0x47B2CF7F, 0x30B5FFE9,
+            0xBDBDF21C, 0xCABAC28A, 0x53B39330, 0x24B4A3A6, 0xBAD03605, 0xCDD70693, 0x54DE5729, 0x23D967BF,
+            0xB3667A2E, 0xC4614AB8, 0x5D681B02, 0x2A6F2B94, 0xB40BBE37, 0xC30C8EA1, 0x5A05DF1B, 0x2D02EF8D,
+        };
+
+        private static uint CalculateCrc32(uint initialCrc, byte[] buffer) {
+            uint crc = initialCrc;
+            foreach (var b in buffer) {
+                crc = Crc32LookupTable[(crc ^ b) & 0xff] ^ (crc >> 8);
+            }
+            return crc;
+        }
+
+        #endregion Png
 
         #region Static
 
--- tests/Tests.Medo/Drawing/BarcodePattern.Tests.cs
+++ tests/Tests.Medo/Drawing/BarcodePattern.Tests.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Xunit;
 using Medo.Drawing;
 using System.Globalization;
+using System.Drawing;
 
 namespace Tests.Medo.Drawing {
     public class BarcodePatternTests {
@@ -52,6 +53,26 @@ namespace Tests.Medo.Drawing {
             });
         }
 
+        [Fact(DisplayName = "BarcodePattern: Codabar Write PNG")]
+        public void CodabarWritePng() {
+            var bp = BarcodePattern.GetNewCodabar("1234");
+            var memStream = new MemoryStream();
+            bp.WritePngImageToStream(memStream);
+            //File.WriteAllBytes(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "codabar-24.png"), memStream.ToArray());
+            Assert.Equal("89504E470D0A1A0A0000000D49484452000000430000000F08020000003032E5330000005749444154789CED95CB0AC0201003E7FF7F3A45A5521F3D55B14AE62041836C5057740AAC2E6018AC2E604212089A88229482724CA2BBDADAB278DBAD32779DD5FC5304AD1B9C049F897CBBF03B71EF92BB30DFFE93DD7192FF71015FA30C3C0000000049454E44AE426082",
+                         BitConverter.ToString(memStream.ToArray()).Replace("-", ""));
+        }
+
+        [Fact(DisplayName = "BarcodePattern: Codabar Write Transparent PNG")]
+        public void CodabarWriteTransparentPng() {
+            var bp = BarcodePattern.GetNewCodabar("1234");
+            var memStream = new MemoryStream();
+            bp.WritePngImageToStream(memStream, Color.Blue, Color.Transparent);
+            //File.WriteAllBytes(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "codabar-32.png"), memStream.ToArray());
+            Assert.Equal("89504E470D0A1A0A0000000D49484452000000430000000F0806000000BF5072640000009749444154789CED93C10AC5200C04E7FF7F7A9EBE5648416D6E2D34874D5C70C02C069592FF0C2A088F206CA2174BF40C8841D0AB47E73C7BFADE0D67269DC43D170C499E05BBE3B899C5E18D8656AD30A830AC9F71D90CA3A155737BC6A493B8E78221C9B360771C37B338BCD1D0AA15061586F5332E9B6134B46A6ECF987412F75C30247916EC8EE366168737183F2E9E7E802F124F3FC017E907EB28F7C90000000049454E44AE426082",
+                         BitConverter.ToString(memStream.ToArray()).Replace("-", ""));
+        }
+
 
         [Fact(DisplayName = "BarcodePattern: Code128 Numbers")]
         public void Code128Numbers() {
@@ -97,6 +118,26 @@ namespace Tests.Medo.Drawing {
             Assert.Throws<ArgumentOutOfRangeException>(() => {
                 var _ = BarcodePattern.GetNewCode128("Č");
             });
+        }
+
+        [Fact(DisplayName = "BarcodePattern: Code128 Write PNG")]
+        public void Code128WritePng() {
+            var bp = BarcodePattern.GetNewCode128("1234");
+            var memStream = new MemoryStream();
+            bp.WritePngImageToStream(memStream);
+            //File.WriteAllBytes(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "code128-24.png"), memStream.ToArray());
+            Assert.Equal("89504E470D0A1A0A0000000D494844520000003F0000000F0802000000DC4780250000006549444154789CED94510AC0300843BDFFA533E947964D1C8C0DA4907C149B3E4A28D6C0CE8AE9009F14D3017E4A1F4B5805C4D12DCD07A08315A83774BC1EA1026775E5E8E896269D0A74B002F5868E777ABF7D2ADB20D75B41C19DE35F0BCF1CBC99983BCAE9E77400667F9C630000000049454E44AE426082",
+                         BitConverter.ToString(memStream.ToArray()).Replace("-", ""));
+        }
+
+        [Fact(DisplayName = "BarcodePattern: Code128 Write Transparent PNG")]
+        public void Code128WriteTransparentPng() {
+            var bp = BarcodePattern.GetNewCode128("1234");
+            var memStream = new MemoryStream();
+            bp.WritePngImageToStream(memStream, Color.Blue, Color.Transparent);
+            //File.WriteAllBytes(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "code128-32.png"), memStream.ToArray());
+            Assert.Equal("89504E470D0A1A0A0000000D494844520000003F0000000F0806000000532517720000007549444154789CED95C10EC0200843FBFF3FFD16B29938C2C261070EF640AC48151051804E154D3B8083672409EF891E0D37CE7A25DDBE1698C696864B71EE8E95F6D6075F451CAA6C29880BC78883C7379FAB61AF8AC034B6345C92AD5CF6F8CD4715C80D0F77FBD513E4AF8EFFFFFC69A2690770F08C24E102AC515D030000000049454E44AE426082",
+                         BitConverter.ToString(memStream.ToArray()).Replace("-", ""));
         }
 
 
