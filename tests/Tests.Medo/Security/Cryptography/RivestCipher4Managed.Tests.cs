976970a367b913e0f4c91b6e26e44e85bb15fa45
Josip Medved <jmedved@jmedved.com>
2022-01-07 01:31:24 -0800

    RivestCipher4: Added more tests

--- tests/Tests.Medo/Security/Cryptography/RivestCipher4Managed.Tests.cs
+++ tests/Tests.Medo/Security/Cryptography/RivestCipher4Managed.Tests.cs
@@ -53,6 +53,46 @@ public class RivestCipher4ManagedTests {
     }
 
 
+    [Theory(DisplayName = "RivestCipher4: Test Vectors")]
+    [InlineData("Vector40A.txt")]
+    [InlineData("Vector56A.txt")]
+    [InlineData("Vector64A.txt")]
+    [InlineData("Vector80A.txt")]
+    [InlineData("Vector128A.txt")]
+    [InlineData("Vector192A.txt")]
+    [InlineData("Vector256A.txt")]
+    [InlineData("Vector40B.txt")]
+    [InlineData("Vector56B.txt")]
+    [InlineData("Vector64B.txt")]
+    [InlineData("Vector80B.txt")]
+    [InlineData("Vector128B.txt")]
+    [InlineData("Vector192B.txt")]
+    [InlineData("Vector256B.txt")]
+    public void Vectors(string fileName) {
+        RetrieveVectors(fileName, out var key, out var dataQueue);
+
+        using var ct = new MemoryStream();
+        using var transform = new RivestCipher4Managed().CreateEncryptor(key, null);
+        using var cs = new CryptoStream(ct, transform, CryptoStreamMode.Write);
+
+        var n = 0;
+        while (dataQueue.Count > 0) {
+            var entry = dataQueue.Dequeue();
+            var index = entry.Key;
+            var expectedBytes = entry.Value;
+
+            while (n <= index) {
+                cs.Write(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });  // hardcoded to 16-bytes
+                n += 16;
+            }
+            cs.Flush();
+            var cipherBytes = new byte[16];
+            Array.Copy(ct.ToArray(), n - 16, cipherBytes, 0, 16);  // not efficient but good enough for test
+            Assert.Equal(BitConverter.ToString(expectedBytes), BitConverter.ToString(cipherBytes));
+        }
+    }
+
+
     #region Private helper
 
     private static byte[] Encrypt(SymmetricAlgorithm algorithm, byte[] key, byte[] iv, byte[] pt) {
@@ -71,6 +111,37 @@ public class RivestCipher4ManagedTests {
         using var ms = new MemoryStream();
         cs.CopyTo(ms);
         return ms.ToArray();
+    }
+
+    private static void RetrieveVectors(string fileName, out byte[] key, out Queue<KeyValuePair<int, byte[]>> data) {
+        using var reader = new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("Tests.Medo._Resources.Security.Cryptography.RivestCipher4." + fileName));
+
+        key = GetBytes(reader.ReadLine().Trim(), out var headerText);
+        if (!headerText.Equals("KEY", StringComparison.InvariantCultureIgnoreCase)) { throw new InvalidDataException(); }
+
+        data = new Queue<KeyValuePair<int, byte[]>>();
+        while (!reader.EndOfStream) {
+            var line = reader.ReadLine();
+            if (line.Length > 0) {
+                var bytes = GetBytes(line, out var locationText);
+                var location = int.Parse(locationText, NumberStyles.HexNumber);
+                data.Enqueue(new KeyValuePair<int, byte[]>(location, bytes));
+            }
+        }
+    }
+
+    private static byte[] GetBytes(string lineText, out string headerText) {
+        var parts = lineText.Split(":");
+        if (parts.Length != 2) { throw new InvalidDataException(); }
+
+        headerText = parts[0].Split(" ", StringSplitOptions.RemoveEmptyEntries)[^1];
+        var bytesText = parts[1].Trim().Replace(" ", "");
+        if (bytesText.StartsWith("0x")) { bytesText = bytesText[2..]; }
+        var data = new Queue<byte>();
+        for (var i = 0; i < bytesText.Length; i += 2) {
+            data.Enqueue(byte.Parse(bytesText.Substring(i, 2), NumberStyles.HexNumber));
+        }
+        return data.ToArray();
     }
 
     #endregion
