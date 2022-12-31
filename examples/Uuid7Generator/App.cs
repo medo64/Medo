bd642626bc8be96aed9037f19d58ab4340d3ab71
Josip Medved <jmedved@jmedved.com>
2022-12-31 13:11:21 -0800

    Added UUID7 example

---
+++ examples/Uuid7Generator/App.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using Medo;
+
+internal static class App {
+
+    public static void Main() {
+        Thread.Sleep(1000);
+
+        {
+            var guidCount = 0;
+            var sw = Stopwatch.StartNew();
+            while (sw.ElapsedMilliseconds < 3000) {
+                _ = Guid.NewGuid();
+                guidCount++;
+            }
+            sw.Stop();
+            Console.WriteLine($"Generated {guidCount:#,##0} GUIDs in {sw.ElapsedMilliseconds:#,##0} millisecond ({guidCount / sw.ElapsedMilliseconds * 1000:#,##0} per second)");
+        }
+
+        Thread.Sleep(1000);
+
+        {
+            var uuidCount = 0;
+            var sw = Stopwatch.StartNew();
+            while (sw.ElapsedMilliseconds < 3000) {
+                _ = Uuid.NewUuid7();
+                uuidCount++;
+            }
+            sw.Stop();
+            Console.WriteLine($"Generated {uuidCount:#,##0} UUIDs in {sw.ElapsedMilliseconds:#,##0} millisecond ({uuidCount / sw.ElapsedMilliseconds * 1000:#,##0} per second)");
+        }
+    }
+
+}
